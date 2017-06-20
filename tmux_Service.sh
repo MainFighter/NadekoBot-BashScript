1fffe4aeb09337b08211ba23b27158b2efe6539c
Main Fighter <mainfighter3302@gmail.com>
2017-06-20 18:02:21 +1000

    added reload

--- tmux_Service.sh
+++ tmux_Service.sh
@@ -26,6 +26,25 @@ stop() {
 	tmux kill-session -t nadeko
 }
 
+reload () {
+	tmux kill-session -t nadeko
+	
+	if [ $parms = "autorestart" ] ; then
+		tmux new-session -d -s nadeko 'curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/1.4/NadekoARN.sh | sh'
+	else
+		if [ $parms = "autorestartupdate" ] ; then
+			tmux new-session -d -s nadeko 'curl -L https://github.com/Kwoth/NadekoBot-BashScript/raw/1.4/NadekoARU_Latest.sh | sh'
+		else
+			echo ""
+			echo "Failed to start..."
+			echo "You must use one of the parms below..."
+			echo "autorestart - Auto Restart only"
+			echo "autorestartupdate - Auto Restart and Update"
+			echo ""
+		fi
+	fi
+}
+
 case $1 in
-	start|stop) "$1" ;;
+	start|stop|reload) "$1" ;;
 esac
